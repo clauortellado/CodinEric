90b63f6b14b17b4984f86c8075dbb35cd459009f
clauortellado <clauortellado@gmail.com>
2020-05-05 17:39:06 -0300

    Juego de ID. Dados

--- test1.py
+++ test1.py
@@ -1,14 +1,34 @@
 # Video 6 - if + Operadores de Comparacion
 
+'''
 num1 = int(input("ingrese 1er nota: "))
 num2 = int(input("ingrese 2da nota:"))
 num3 = int(input("ingrese 3er nota:"))
 
 prom = ( num1 + num2 + num3)/3
 
-print("el PROMEDIO es %f"%prom)
+print("el PROMEDIO es %.2f"%prom)
 
 if(prom > 6):
     print("Aprobado")
 else:
     print("Desaprobado")
+'''
+# Video 7 - while
+
+num = int(input("ingresar nota: "))
+
+total = 0
+cant = 0
+while(num != 0):
+    if (num > 0 and num < 11):
+    total = total + num
+    cant = cant + 1
+    num = int(input("ingresar nota: "))
+else:
+    num = int(input("ingresar nota entre 1 y 10 - 0 para salir "))
+
+print("suma notas: %.2f - cant notas: %i"% (total,cant))
+print("cant notas: %i"%cant)
+prom = total/cant
+print("promedio: %.2f"%prom)
---
+++ test3.py
@@ -0,0 +1,67 @@
+# Video 8
+'''
+Tirar 4 veces un dado1Eliminar el Valor menor
+y Sumar el Total restante
+'''
+
+import random
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1, dado2, dado3, dado4)
+Fuerza = suma - menor
+#print("dado1: %i - dado2: %i - dado3 %i - dado4: %i"% (dado1, dado2, dado3, dado4))
+print("Fuerza: %i"% Fuerza)
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1, dado2, dado3, dado4)
+Destreza = suma - menor
+#print("dado1: %i - dado2: %i - dado3 %i - dado4: %i"% (dado1, dado2, dado3, dado4))
+print("Destreza: %i"% Destreza)
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1, dado2, dado3, dado4)
+Constitucion = suma - menor
+#print("dado1: %i - dado2: %i - dado3 %i - dado4: %i"% (dado1, dado2, dado3, dado4))
+print("Constitucion: %i"% Constitucion)
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1, dado2, dado3, dado4)
+Inteligencia = suma - menor
+#print("dado1: %i - dado2: %i - dado3 %i - dado4: %i"% (dado1, dado2, dado3, dado4))
+print("Inteligencia: %i"% Inteligencia)
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1, dado2, dado3, dado4)
+Sabiduria = suma - menor
+#print("dado1: %i - dado2: %i - dado3 %i - dado4: %i"% (dado1, dado2, dado3, dado4))
+print("Sabiduria: %i"% Sabiduria)
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1, dado2, dado3, dado4)
+Carisma = suma - menor
+#print("dado1: %i - dado2: %i - dado3 %i - dado4: %i"% (dado1, dado2, dado3, dado4))
+print("Carisma: %i"% Carisma)
