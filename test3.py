ece0ee2fe273177af0ff6dddd4ec312d3a13f3b3
clauortellado <clauortellado@gmail.com>
2020-05-06 11:07:06 -0300

    Juego de ID

--- test3.py
+++ test3.py
@@ -6,6 +6,7 @@ y Sumar el Total restante
 
 import random
 
+'''
 dado1 = random.randint(1,6)
 dado2 = random.randint(1,6)
 dado3 = random.randint(1,6)
@@ -65,3 +66,19 @@ menor = min(dado1, dado2, dado3, dado4)
 Carisma = suma - menor
 #print("dado1: %i - dado2: %i - dado3 %i - dado4: %i"% (dado1, dado2, dado3, dado4))
 print("Carisma: %i"% Carisma)
+'''
+
+mis_dados = [4,5,6,2]
+print(mis_dados)
+mis_dados.sort()
+print(mis_dados)
+print("option1___________")
+dados_min = min(mis_dados)
+print(dados_min)
+altos = sum(mis_dados)-dados_min
+print(altos)
+print("option2___________")
+dados_altos = mis_dados[1:]
+print(dados_altos)
+altos = sum(dados_altos)
+print(altos)
